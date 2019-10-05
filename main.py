9c3cd77d97d3e845bf5a89652d671f84f2173279
Japnix <brendan.tawa@gmail.com>
2019-10-05 01:59:20 -0400

    Working on API creation

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from fftcg_parser import *
 import json
+from flask import Flask, escape, request
+from stringify import *
 
 cards = loadJson('https://fftcg.square-enix-games.com/en/get-cards')
+
+app = Flask(__name__)
 
 mykeys = ('Element','Name_EN','Cost','Code','Multicard','Type_EN','Category_1','Text_EN','Job_EN','Power', 'Ex_Burst' , 'Set', 'Rarity')
 
@@ -15,7 +19,20 @@ for card in cards:
 with open('cards.json', 'w') as outfile:
     json.dump(cards, outfile)
 
-print(cards[0])
+myjson = json.dumps(cards)
+mydict = json.loads(myjson)
+
+@app.route('/card/<name>')
+def hello(name):
+    for card in mydict:
+        if card['Code'] == name:
+            return card
+
+@app.route('/')
+def hello2():
+    return myjson
 
 
 
+if __name__ == '__main__':
+    app.run()
