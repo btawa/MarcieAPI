b3ca7c7bee039af8a4dc12377e719cdeeca5918c
btawa <brendan.tawa@gmail.com>
2019-10-31 10:24:00 -0400

    Don't write a file unless ffdecks or square

--- makejson.py
+++ makejson.py
@@ -6,15 +6,19 @@ if sys.argv[1] == 'ffdecks':
     ffdecks = loadJson('https://ffdecks.com/api/cards/basic')
     cards = ffdeckstomarcieapi(ffdecks['cards'])
     imageurlset = urlset(cards)
+    cards = addimageurltojson(cards, imageurlset)
+
+    with open('cards.json', 'w+') as outfile:
+        json.dump(cards, outfile)
 
 elif sys.argv[1] == 'square':
     square = loadJson('https://fftcg.square-enix-games.com/en/get-cards')
     cards = squaretomarcieapi(square['cards'])
     imageurlset = urlset(cards)
+    cards = addimageurltojson(cards, imageurlset)
+
+    with open('cards.json', 'w+') as outfile:
+        json.dump(cards, outfile)
+
 else:
     pass
-
-cards = addimageurltojson(cards, imageurlset)
-
-with open('cards.json', 'w+') as outfile:
-    json.dump(cards, outfile)
